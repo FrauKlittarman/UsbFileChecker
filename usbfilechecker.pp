66b280a0f7699cdf4f9c7a37cde8974007a9ac2e
FrauKlitterman <mrsir.san@gmail.com>
2026-04-17 15:03:21 +0700

    fix usbfilechecker.pp

--- usbfilechecker.pp
+++ usbfilechecker.pp
@@ -1,11 +1,10 @@
 #usbfilechecker
 class usbfilechecker (
-  String $source_filename = 'FILENAME.xls',
-  Array[String] $packages = ['python3‑toml'],
-  String $minute = '*/5',
+  String $source_filename = 'test.xls',
+  String $minute = '*/10',
+  Array[String] $hours = ['7-19'],
 ){
-
-    ensure_resources('package', { $packages => {} }, { 'ensure' => 'present' })
+    ensure_packages ( ['python3-toml',] )
 
     file { '/var/local/usbfilechecker/':
       ensure  => directory,
@@ -16,19 +15,30 @@ class usbfilechecker (
       mode    => '0750',
       purge   => true,
       force   => true,
+      ignore  => ['__pycache__', 'pyproject.toml'],
+    }
+
+    file { '/var/local/usbfilechecker/pyproject.toml':
+      ensure  => directory,
+      source  => 'puppet:///modules/usbfilechecker/pyproject.toml',
+      owner   => root,
+      group   => root,
+      mode    => '0640',
+      replace => false,
     }
 
     file_line { 'set source filename':
       ensure  => present,
       path    => '/var/local/usbfilechecker/pyproject.toml',
-      match   => '^filename =',
-      line    => "filename = \"${source_filename}\""
+      match   => '^filename=',
+      line    => "filename=\"${source_filename}\""
     }
 
     cron { 'run usbfilechecker':
       ensure  => present,
       command => "/usr/bin/python3 /var/local/usbfilechecker/main.py > /dev/null 2>&1",
       user    => root,
+      hour    => $hours,
       minute  => $minute,
     }
 }
